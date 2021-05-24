a95612ecf970075688171b481c10b418a8a5a7c6
William Rodrigues <williamrodrigues2442@gmail.com>
2021-05-24 06:11:21 -0300

    :sparkles: Add spinner after sending e-mail

--- app/static/script.js
+++ app/static/script.js
@@ -9,43 +9,63 @@ const closeButton = document.querySelector('.close-button');
 const form = document.querySelector('.main-form');
 const emailInput = document.querySelector('#email');
 const helpMessage = document.querySelector('.help');
+const sendButton = document.querySelector('.main-button');
 
-// Add the message element, and show the message.
-function showFlashMessage(message, category) {
-  const messageElement = `<p class="message-category  -${category}">${message}</p>`;
+// Add and show the flash message.
+function showFlashMessage(message, emailWasSent) {
+  const variant = emailWasSent ? '-success' : '-error';
+  const messageElement = `<p class="message-category  ${variant}">${message}</p>`;
 
   divMessages.insertAdjacentHTML('afterbegin', messageElement);
   divMessages.style.display = 'flex';
 }
 
-// Remove the message element, and close the flash message.
+// Remove and close the flash message.
 function closeFlashMessage() {
   divMessages.removeChild(divMessages.firstChild);
   divMessages.style.display = 'none';
 }
 
+// Add and show the spinner.
+function showSpinner() {
+  const spinner = '<span class="fas fa-spinner fa-spin"></span>';
+
+  sendButton.textContent = '';
+  sendButton.disabled = true;
+  sendButton.insertAdjacentHTML('afterbegin', spinner);
+}
+
+// Remove and hide the spinner.
+function hideSpinner() {
+  sendButton.removeChild(sendButton.firstChild);
+  sendButton.textContent = 'Enviar';
+  sendButton.disabled = false;
+}
+
 form.addEventListener('submit', event => {
   // Prevent default, don't refresh the page.
   event.preventDefault();
+
+  showSpinner();
   
   const data = new FormData(form);
 
   fetch(`${location.origin}/send-email`, { method: 'POST', body: data })
-  .then(response => response.json())
+  .then(response => response.ok ? response.json() : Promise.reject(response))
   .then(json => {
-    const message = json.message;
-    const messageCategory = json.sent ? 'success' : 'error';
-
-    // Show the message.
-    showFlashMessage(message, messageCategory);
-
     // Reset the form to default (blank form).
     form.reset();
+
+    showFlashMessage(json.message, json.sent);
+    hideSpinner();
 
     // Set timout to close the message automatically after 10 sec.
     timeout = setTimeout(closeFlashMessage, 10000);
   })
-  .catch(err => console.error(err));
+  .catch(err => {
+    hideSpinner();
+    console.error(`${err.status}: ${err.statusText}`);
+  });
 });
 
 closeButton.addEventListener('click', () => {
